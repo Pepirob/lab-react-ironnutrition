a0c45336054c4b797f8896918944563c7c0ed32a
Jose Luis <pepirob>
2023-03-01 20:00:03 +0100

    Iteration 5 completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import AddFoodForm from './components/AddFoodForm';
+import FoodBox from './components/FoodBox';
+import Search from './components/Search';
+import foods from './foods.json';
 
 function App() {
+  const [food, setFood] = useState(foods);
+  const [foodToDisplay, setFoodToDisplay] = useState(foods);
+
+  const filterFood = (searchQuery) => {
+    const filteredFood = food.filter((food) => {
+      const lowCaseQuery = searchQuery.toLowerCase();
+      const lowCaseName = food.name.toLowerCase();
+      if (lowCaseName.includes(lowCaseQuery)) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+    setFoodToDisplay(filteredFood);
+  };
+
+  const deleteFood = (event) => {};
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AddFoodForm setFood={setFood} setFoodToDisplay={setFoodToDisplay} />
+      <Search filterFood={filterFood} />
+      {foodToDisplay.map((meal) => (
+        <FoodBox
+          key={meal.name}
+          food={{
+            name: meal.name,
+            calories: meal.calories,
+            image: meal.image,
+            servings: meal.servings,
+          }}
+        />
+      ))}
     </div>
   );
 }
